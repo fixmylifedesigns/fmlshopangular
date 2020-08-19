39a8a89543f854171b1e7170a1ea6a8061ea3dcc
fixmylifedesigns <ijd.irving@gmail.com>
2020-08-18 20:30:42 -0400

    title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = "angular app";
+  title = "hello";
   onActivate(event) {
     let scrollToTop = window.setInterval(() => {
       let pos = window.pageYOffset;
